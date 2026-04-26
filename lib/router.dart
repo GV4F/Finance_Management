a5eac747b4481462fec7e160ce1b147aab4372da
Ángel Caal <angelcaal0619@gmail.com>
2026-04-25 18:47:13 -0600

    Initial State

---
+++ lib/router.dart
@@ -0,0 +1,28 @@
+import 'package:go_router/go_router.dart';
+
+// * PAGES
+import './pages/home_page.dart';
+import './pages/historial_page.dart';
+import './pages/savings_page.dart';
+import './pages/profile_page.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      path: '/historial',
+      builder: (context, state) => const HistorialPage(),
+    ),
+    GoRoute(
+      path: '/savings',
+      builder: (context, state) => const SavingsPage(),
+    ),
+    GoRoute(
+      path: '/profile',
+      builder: (context, state) => const ProfilePage(),
+    )
+  ]
+);
