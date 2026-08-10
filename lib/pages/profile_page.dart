e0a2987f7b2e11572c08ecf30a96cf989e2858cc
Ángel Caal <angelcaal0619@gmail.com>
2026-08-10 17:39:37 -0600

    Add: Profile section has been created; Only I need implement Profile pic function

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // * Widgets
 import 'package:gvaf_finance_app/widgets/header_page.dart';
+import 'package:gvaf_finance_app/widgets/profile_section.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -11,7 +12,9 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          HeaderPage(titlePage: 'Profile')
+          HeaderPage(titlePage: 'Profile'),
+          SizedBox(height: 20),
+          ProfileSection(),
         ],
       ),
     );
---
+++ lib/widgets/profile_section.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import '../auth/auth_service.dart';
+
+class ProfileSection extends StatefulWidget {
+  const ProfileSection({super.key});
+
+  @override
+  State<ProfileSection> createState() => _ProfileSectionState();
+}
+
+Future<void> signOut() async{
+  final auth = AuthService();
+  await auth.signOut();
+}
+
+class _ProfileSectionState extends State<ProfileSection> {
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(
+        vertical: 30.0,
+        horizontal: 24.0,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 1),
+          ),
+        ],
+      ),
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF2C2C34).withValues(alpha: 0.4),
+                  border: Border.all(
+                    color: Colors.white.withValues(alpha: 0.05),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 8),
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundColor: colors.surface,
+                      backgroundImage: AssetImage('/assets/images/profile_picture.jpg'),
+                      child: const Icon(Icons.person, size: 55, color: Colors.white),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              signOut();
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                              decoration: BoxDecoration(
+                                color: colors.tertiary,
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(
+                                  color: colors.onSurface.withValues(alpha: 0.08),
+                                ),
+                              ),
+                              child: Text(
+                                'Log Out',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(width: 12), 
+
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              // Acción botón Security
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                              decoration: BoxDecoration(
+                                color: colors.secondary,
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(
+                                  color: Colors.white.withValues(alpha: 0.08),
+                                ),
+                              ),
+                              child: Text(
+                                'Edit Photo',
+                                style: TextStyle(
+                                  color: colors.onPrimary,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]
+                    )
+                  ]
+                )
+              )
+            )
+          )
+        )
+      );
+  }
+}
