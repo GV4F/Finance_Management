3df41e795a91d0666aa7e599b166c512d61b7d67
Ángel Caal <angelcaal0619@gmail.com>
2026-08-10 22:15:34 -0600

    Ignore in formatAmount

--- lib/utils/formatAmount.dart
+++ lib/utils/formatAmount.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: file_names
+
 import 'package:intl/intl.dart';
-  String formatAmount(double value) {
-    final formatCurrency = NumberFormat.currency(locale: 'es_GT', symbol: 'Q', customPattern: '¤#,##0.00');
-    return formatCurrency.format(value);
-  }
+
+String formatAmount(double value) {
+  final formatCurrency = NumberFormat.currency(
+    locale: 'es_GT',
+    symbol: 'Q',
+    customPattern: '¤#,##0.00',
+  );
+  return formatCurrency.format(value);
+}
